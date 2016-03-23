631cbdc3a03cd3cd3d0021ada7f1bc02e3988c52
Gail Matsushima <gail.matsushima@gmail.com>
2016-03-22 21:41:09 -1000

    re-factoring to have reddit topics searched and listed dynamically vs hard coded

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,13 @@
 
-var PuppyBox = React.createClass({
+var RedditBox = React.createClass({
   getInitialState: function() {
-    return { data: [] };
+    return { data: [], redditTopic: '' };
   },
 
-  loadPuppiesFromServer: function(e) {
+  loadRedditFromServer: function(e) {
 
     $.ajax({
-      url: "https://www.reddit.com/r/" + "aww" + ".json",
+      url: "https://www.reddit.com/r/" + redditTopic + ".json",
       method: "GET",
       dataType: "json",
       success: function(data) {
@@ -17,51 +17,93 @@ var PuppyBox = React.createClass({
     });
   },
 
+  handleInputSubmit: function(topic) {
+
+    $.ajax({
+      url: "https://www.reddit.com/r/" + redditTopic + ".json",
+      method: 'POST',
+      dataType: 'json',
+      data: topic,
+      success: function(data) {
+        this.setState({ data: this.state.data.concat(data) });
+      }.bind(this)
+    });
+  },
+
   componentDidMount: function() {
-    this.loadPuppiesFromServer();
-    setInterval(this.loadPuppiesFromServer, 5000);
+    this.loadRedditFromServer();
+    setInterval(this.loadRedditFromServer, 5000);
   },
 
   render: function() {
     return (
-      <div className="puppyBox">
+      <div className="redditBox">
         <RedditForm
-          data={this.state.data} />
+          onTopicSubmt={this.handleInputSubmit}
+        />
+        <RedditList
+          data={this.state.data}
+        />
       </div>
     );
   }
 });
 
 var RedditForm = React.createClass({
+  getInitialState: function() {
+    return { redditTopic: ''};
+  },
 
-  // handleUrlChange: function(e) {
-  //   this.setState({ url: "https://www.reddit.com/r/" + e.target.value + ".json"})
-  // },
+  handleRedditChange: function(e) {
+    this.setState({ redditTopic: e.target.value })
+  },
 
-  // handleSubmit: function(e) {
-  //   e.preventDefault();
-  //   var url = this.state.url;
-  //   return this.state.url;
-  // },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var redditTopic = this.state.redditTopic;
+
+    this.props.onTopicSubmit({ redditTopic: redditTopic });
+    this.setState({ redditTopic: '' });
+
+  },
 
   render: function() {
-    var puppyNodes = this.props.data.map(function(puppy, index) {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Reddit Topic"
+          value={this.state.redditTopic}
+          onChange={this.handleRedditChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+});
+
+
+
+var RedditList = React.createClass({
+
+  render: function() {
+    var redditNodes = this.props.data.map(function(reddit, index) {
       return(
         <div key={index}>
-          <p><a href={puppy.data.url}>{puppy.data.url}</a></p>
-          <img src={puppy.data.thumbnail} />
+          <p><a href={reddit.data.url}>{reddit.data.url}</a></p>
+          <img src={reddit.data.thumbnail} />
         </div>
       );
     });
     return (
       <div>
-       {puppyNodes}
+       {redditNodes}
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <PuppyBox />,
+  <RedditBox />,
   document.getElementById('content')
 );
